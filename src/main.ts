4d15a4f0d9f77286289e3d953aa6df411e7d6e91
Dmitry <andrewgamer63@gmail.com>
2023-12-30 19:57:17 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+const YUHbtn = document.querySelector<HTMLButtonElement>(".YUH");
+const kittyIMG = document.querySelector<HTMLImageElement>(".kitty");
+
+const miau: HTMLAudioElement = new Audio("./Miau.mp3");
+miau.volume = 0.1;
+
+const getRandomInt = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+YUHbtn!.onclick = () => {
+  kittyIMG!.srcset = `kitty${getRandomInt(1, 4)}.jpg`;
+
+  miau.play();
+
+  kittyIMG!.animate(
+    [
+      { transform: "scale(0)", opacity: 0 },
+      { opacity: 1 },
+      { opacity: 1 },
+      { transform: "scale(6)" },
+    ],
+    {
+      duration: 5000,
+      easing: "ease-out",
+    }
+  );
+};
